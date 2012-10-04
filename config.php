f4b038534392e59c174f131392550217d5844362
snghrym <i@snghrym.com>
2012-10-04 21:38:21 +0900

    2.0.0 初期コミット

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('APP_ID','490580820960694');
 define('APP_SECRET','d8117be64300a21c62f8f201a1e5c6a1');
 
 //ユーザー登録時のメールアドレス
-define('MAIL','hirayamaru@gmail.com');
+//define('MAIL','hirayamaru@gmail.com');
 
 
 
